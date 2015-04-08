c473c485ce2f1ec714121f33f0d21aef49bd5725
Orconsulting <Orconsulting@MacBook-Pro.local>
2015-04-08 21:10:41 +0200

    Initial Commit

---
+++ NoNamedPrototype/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  NoNamedPrototype
+//
+
+//  Copyright (c) 2015 Orconsulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
